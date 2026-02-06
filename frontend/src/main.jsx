a63cabbaca35f4f90e219388496427819f84240d
M.K.Thiong'o <kamauthiongo58@gmail.com>
2026-02-06 08:17:27 +0300

    Refactor main.jsx to remove BrowserRouter
    Removed BrowserRouter wrapper around App component.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './main.css'
-import { store } from './redux/store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
+import App from './App.jsx';
+import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>,
-)
-
+);
